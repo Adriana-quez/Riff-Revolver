06e82611105f2f2ef54ae930e1796300568802b1
peleg7201 <peleg.merin@gmail.com>
2024-11-06 11:29:20 -0500

    Added basics of score and multiplier

--- Riff-Revolver/Assets/Scripts/GameManager.cs
+++ Riff-Revolver/Assets/Scripts/GameManager.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
+    public int currentScore = 0;
+    public int scorePerNote = 100;
+
+    public int currentMultiplier;
+    public int multiplierTracker;
+    public int[] multiplierThresholds;
+
+    public TMP_Text scoreText;
+    public TMP_Text multiplierText;
+
     void Start()
     {
         Instance = this;
+        scoreText.text = "Score: 0";
+        currentMultiplier = 1;
     }
 
     void Update()
@@ -19,10 +32,29 @@ public class GameManager : MonoBehaviour
     public void NoteHit()
     {
         Debug.Log("Hit");
+
+        if (currentMultiplier - 1 < multiplierThresholds.Length)
+        {
+            multiplierTracker++;
+            if (multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+            }
+        }
+
+        multiplierText.text = "Multiplier: x" + currentMultiplier;
+
+        currentScore += scorePerNote * currentMultiplier;
+        scoreText.text = "Score: " + currentScore.ToString();
     }
 
     public void NoteMissed()
     {
         Debug.Log("Miss");
+        currentMultiplier = 1;
+        multiplierTracker = 0;
+
+        multiplierText.text = "Multiplier: x" + currentMultiplier;
     }
 }
