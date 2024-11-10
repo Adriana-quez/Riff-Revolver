fe2e0a9d0b259f102ceb666d0daee460b8fdd770
peleg7201 <peleg.merin@gmail.com>
2024-11-10 18:07:04 -0500

    Swapped roles of hit line and note

--- Riff-Revolver/Assets/Scripts/Note.cs
+++ Riff-Revolver/Assets/Scripts/Note.cs
@@ -5,18 +5,74 @@ using UnityEngine;
 public class Note : MonoBehaviour
 {
     public float moveSpeed = 2f;
+    public int trackNumber;
+    private GameObject hitLine;
+    private NoteDestroyer noteDestroyer;
+    private float hitLineCenter;
+    public KeyCode destroyKey;
+    public bool touching;
+    public bool scored;
     void Start()
     {
-        
+        hitLine = GameObject.FindWithTag("HitLine");
+        hitLineCenter = hitLine.transform.position.x;
+        destroyKey = KeyCode.Space;
+        touching = false;
+        scored = false;
+        noteDestroyer = hitLine.GetComponent<NoteDestroyer>();
     }
 
     void Update()
     {
         transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(destroyKey) && touching)
+        {
+            float currentNotePosFromCenter = Mathf.Abs(transform.position.x - hitLineCenter);
+
+            if (currentNotePosFromCenter <= noteDestroyer.getPerfectTolerance())
+            {
+                GameManager.Instance.PerfectHit();
+            }
+            else if (currentNotePosFromCenter > noteDestroyer.getPerfectTolerance() && currentNotePosFromCenter <= noteDestroyer.getGoodTolerance())
+            {
+                GameManager.Instance.GoodHit();
+            }
+            else
+            {
+                GameManager.Instance.NormalHit();
+            }
+
+            scored = true;
+            Destroy(gameObject);
+        }
     }
 
     private void OnBecameInvisible()
     {
+        if (!scored) GameManager.Instance.NoteMissed();
         Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("HitLine"))
+        {
+            touching = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        // Debug.Log("Note not touching hit line...");
+        // Debug.Log("Note after trigger exit: " + scored);
+        if (other.CompareTag("HitLine"))
+        {
+            touching = false;
+            if (!scored)
+            {
+                GameManager.Instance.NoteMissed();
+            }
+        }
     }
 }
--- Riff-Revolver/Assets/Scripts/NoteDestroyer.cs
+++ Riff-Revolver/Assets/Scripts/NoteDestroyer.cs
@@ -2,19 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class NoteDestroyer : MonoBehaviour
 {
-    public bool noteIsTouching;
-    public KeyCode destroyKey = KeyCode.Space;
-    private GameObject currentNote;
-    private bool isDestroyed;
-
     private static float hitLineCenter;
     public float perfectTolerance;
     public float goodTolerance;
-    public float normalTolerance;
     void Start()
     {
         hitLineCenter = transform.position.x;
@@ -24,45 +17,9 @@ public class NoteDestroyer : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(destroyKey) && noteIsTouching)
-        {
-            float currentNotePosFromCenter = Mathf.Abs(currentNote.transform.position.x - hitLineCenter);
-            
-            if (currentNotePosFromCenter <= perfectTolerance)
-            {
-                GameManager.Instance.PerfectHit();
-            } else if (currentNotePosFromCenter > perfectTolerance && currentNotePosFromCenter <= goodTolerance)
-            {
-                GameManager.Instance.GoodHit();
-            } else
-            {
-                GameManager.Instance.NormalHit();
-            }
-
-            isDestroyed = true;
-            noteIsTouching = false;
-            Destroy(currentNote);
-        }
+        
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Note"))
-        {
-            noteIsTouching = true;
-            currentNote = other.gameObject;
-            isDestroyed = false;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        Debug.Log("is note destroyed: " + isDestroyed);
-        if (other.CompareTag("Note") && !isDestroyed)
-        {
-            noteIsTouching = false;
-            currentNote = null;
-            GameManager.Instance.NoteMissed();
-        }
-    }
+    public float getPerfectTolerance() { return perfectTolerance; }
+    public float getGoodTolerance() { return goodTolerance; }
 }
