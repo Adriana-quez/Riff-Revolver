e83a235ee87ca50992d6582c02c690994a9a6fb3
Adriana Vasquez <99279815+Adriana-quez@users.noreply.github.com>
2024-12-11 09:00:49 -0500

    Level functionality

--- Riff-Revolver/Assets/Scripts/MainMenu&LevelSelect/LevelButtons.cs
+++ Riff-Revolver/Assets/Scripts/MainMenu&LevelSelect/LevelButtons.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LevelButtons : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class LevelButtons : MonoBehaviour
     public static bool level3Unlocked = false;
     public Button twoButton;
     public Button threeButton;
+    [SerializeField] public Button oneButton;
     void Start()
     {
         
@@ -22,13 +24,24 @@ public class LevelButtons : MonoBehaviour
         levelsMenu.SetActive(true);
         twoButton.interactable = true;
         threeButton.interactable = true;
+
+        oneButton.onClick.AddListener(changeButtonOneRouteLevel);
+        twoButton.onClick.AddListener(changeButtonTwoRouteLevel);
+        threeButton.onClick.AddListener(changeButtonThreeRouteLevel);
     }
 
     public void ShowCampaign()
     {
         modeMenu.SetActive(false);
         levelsMenu.SetActive(true);
-        if (!level2Unlocked) 
+        twoButton.interactable = true;
+        threeButton.interactable = true;
+
+        oneButton.onClick.AddListener(changeButtonOneRouteIntro);
+        twoButton.onClick.AddListener(changeButtonTwoRouteIntro);
+        threeButton.onClick.AddListener(changeButtonThreeRouteIntro);
+
+        /*if (!level2Unlocked) 
         {
             twoButton.interactable = false;
             threeButton.interactable = false;
@@ -41,12 +54,36 @@ public class LevelButtons : MonoBehaviour
         else {
             twoButton.interactable = true;
             threeButton.interactable = true;
-        }
+        }*/
     }
 
     public void BackToModeMenu(){
 
         modeMenu.SetActive(true);
         levelsMenu.SetActive(false);
+    }
+
+    void changeButtonOneRouteIntro() {
+        SceneManager.LoadScene("IntroCutscene");
+    }
+
+    void changeButtonOneRouteLevel() {
+        SceneManager.LoadScene("DuelLevel");
+    }
+
+    void changeButtonTwoRouteIntro() {
+        SceneManager.LoadScene("BarIntroScene");
+    }
+
+    void changeButtonTwoRouteLevel() {
+        SceneManager.LoadScene("BarLevel");
+    }
+
+    void changeButtonThreeRouteIntro() {
+        SceneManager.LoadScene("CherryIntroScene");
+    }
+
+    void changeButtonThreeRouteLevel() {
+        SceneManager.LoadScene("CherrySpittingLevel");
     }
 }
